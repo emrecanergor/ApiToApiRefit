8c0cb2f81030dc2b324ae40c628a3b3fc13932f7
emrecanergor <emrecan@rise-consulting.net>
2019-10-21 16:38:35 +0300

    Add project files.

---
+++ WebRefit/Refit/IGitHubApi.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRefit.Refit
+{
+    public interface IGitHubApi
+    {
+        [Get("/users/{user}")]
+        Task GetUser(string user);
+    }
+}
